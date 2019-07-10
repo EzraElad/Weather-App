ad7c276dfe1d76e7b01e717ab603456e0fa59d02
Elad Ezra <xxeladxx@gmail.com>
2019-07-10 15:26:52 +0300

    first Cheackpoint

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const path = require('path')
+const api = require('./server/routes/api')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(express.static(path.join(__dirname, 'node_modules')))
+app.use(express.static(path.join(__dirname, 'dist')))
+app.use( '/', api )
+
+
+mongoose.connect('mongodb://localhost/weatherDB' ,  { useNewUrlParser: true })
+
+
+
+// The Server is Listning
+const port = 3000
+app.listen(port , function(){
+    console.log('The Server is up and running on ' + port)
+})
